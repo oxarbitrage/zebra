8bb78875d17e2ec83f4ab53b4555f962d896a289
Alfredo Garcia <oxarbitrage@gmail.com>
2021-07-27 15:19:22 -0300

    add doc

--- zebra-chain/src/block.rs
+++ zebra-chain/src/block.rs
@@ -148,6 +148,10 @@ impl Block {
 
     /// Get all the value balances from this block by summing all the value balances
     /// in each transaction the block has.
+    ///
+    /// `utxos` must contain the utxos of every input in the transaction,
+    /// including UTXOs created by a transaction in this block,
+    /// then spent by a later transaction that's also in this block.
     pub fn value_balance(
         &self,
         utxos: &HashMap<transparent::OutPoint, transparent::OrderedUtxo>,
